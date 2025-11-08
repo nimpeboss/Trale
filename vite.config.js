f3a3211f161c065972f4926605a2a5e0e80af421
nimpeboss <rivan.azawi@tuni.fi>
2025-11-08 19:25:05 +0200

    now back to working

--- vite.config.js
+++ vite.config.js
@@ -1,64 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Pokemon Higher or Lower - Trale',
-        short_name: 'Pokemon Battle',
-        description: 'An engaging Pokemon guessing game where you compare stats and test your Pokemon knowledge.',
-        theme_color: '#764ba2',
-        background_color: '#667eea',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icons/pokeball.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          },
-          {
-            src: 'icons/pokeball.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/pokeapi\.co\/api\/v2\//,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'pokeapi-cache',
-              expiration: {
-                maxEntries: 200,
-                maxAgeSeconds: 60 * 60 * 24 * 7 // 1 week
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/raw\.githubusercontent\.com\/PokeAPI\/sprites\//,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'pokemon-images-cache',
-              expiration: {
-                maxEntries: 500,
-                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
-              }
-            }
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   base: '/Trale/',
   build: {
     minify: 'terser',
